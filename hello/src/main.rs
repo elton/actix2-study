a901942c6b3b42db44edd79b33d2a2193b1d12b1
Elton Zheng <elton.zheng.sh@gmail.com>
2020-09-03 11:33:19 +0800

    A hello world demo for actix-web.

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,13 +1,22 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use listenfd::ListenFd;
 
-async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+#[get("/hello")]
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hey, there again!")
 }
 
+// 该宏在actix运行时中执行标记的异步函数。该宏可以标记并执行任何异步函数。
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().route("/", web::get().to(index)))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    let mut listenfd = ListenFd::from_env();
+    let mut server = HttpServer::new(|| App::new().service(index2));
+
+    server = if let Some(l) = listenfd.take_tcp_listener(0).unwrap() {
+        server.listen(l)?
+    } else {
+        server.bind("127.0.0.1:8080")?
+    };
+
+    server.run().await
 }
